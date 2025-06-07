bc01228674c1965e09b9f0ed1ee84203a44d30ea
hruday <hruday18@gmail.com>
2025-06-07 02:35:28 -0400

    Added placeholder functions for search and filter

--- taskmanager.py
+++ taskmanager.py
@@ -6,3 +6,7 @@ def delete_task(task_id):
     print(f"Deleting task {task_id}")
 def save_task():
     print("Tasks are auto-saved to local Json")
+def search_tasks(query):
+    print(f"searching for tasks matching: {query}")
+def filter_tasks(criteria):
+    print(f"Filtering tasks by {criteria}")
