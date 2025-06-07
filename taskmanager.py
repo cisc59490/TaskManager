c24c145e7ebaa135aa6fcbaef755af60d0d21e53
hruday <hruday18@gmail.com>
2025-06-07 00:55:49 -0400

    Second commit: Basic taskmanager structure

---
+++ taskmanager.py
@@ -0,0 +1,8 @@
+def add_task(title, priority, due date):
+    print(f"Adding task: {title} (Priority: {priority}, Due: {due_date})")
+def complete_task(task_id):
+    print(f"Completing task {task_id}")
+def delete_task(task_id):
+    print(f"Deleting task {task_id}")
+def save_task():
+    print("Tasks are auto-saved to local Json")
